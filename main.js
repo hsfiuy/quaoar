5211363e0f51a2a9fd6d9a54f9886a675315b2f7
hsfiuy <86751505+hsfiuy@users.noreply.github.com>
2021-11-29 17:02:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach( 'camera' );
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_img" src="' + data_uri + '" />';
+        
+    });
+}
+console.log("ml5 version: ", ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xrO713G27/model.json', modelLoaded);
+function modelLoaded(){
+    console.log('model Loaded')
+}
+
+function check(){
+    img = document.getElementById("capture_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result){
+if (error){
+    console.log(error);
+}else{
+    console.log(result);
+    document.getElementById("result_object_name").innerHTML=results[0].label; 
+    document.getElementById("result_objects_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
